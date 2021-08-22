db09069a60213fba70d400397a256d4892890307
acyadigar <alpercanyadigar@gmail.com>
2021-08-22 09:44:51 +0300

    Change url with dynamic one

--- client/src/components/Chat.vue
+++ client/src/components/Chat.vue
@@ -4,7 +4,7 @@ import { io } from 'socket.io-client'
 export default {
   data() {
     return {
-      socket: io('http://localhost:3000'),
+      socket: io(this.apiUrl),
       user: '',
       message: '',
       messages: [],
